9f48004498ecec1db531e1f9ab3fd57e5a7931af
catalinavele3 <c.velezp2@uniandes.edu.co>
2026-05-10 13:25:54 -0500

    docker

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host="0.0.0.0", port=port)
