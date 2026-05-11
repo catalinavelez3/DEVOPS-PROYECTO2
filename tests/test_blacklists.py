81079ab293ed0afb583a2630565f5636e2722509
catalinavele3 <c.velezp2@uniandes.edu.co>
2026-05-10 21:29:28 -0500

    CI exitoso + CI fallido

--- tests/test_blacklists.py
+++ tests/test_blacklists.py
@@ -41,7 +41,7 @@ def auth_headers():
 
 
 def test_health_returns_200(client):
-    response = client.get("/healthzs")
+    response = client.get("/healthz")
     assert response.status_code == 200
     assert response.get_json()["status"] == "ok"
 
